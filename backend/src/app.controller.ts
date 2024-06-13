458653363d7fc980ddd7a734054144a23502ad40
Kerolous Amged <kerolous.amged@nagwa.com>
2024-06-13 08:44:22 +0300

    update(docker-compose.dev): added mongodb

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -5,8 +5,20 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/')
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/hello')
+  getHello2() {
+    const res = {
+      dbUser: process.env.MONGO_INITDB_ROOT_USERNAME,
+      dbPass: process.env.MONGO_INITDB_ROOT_PASSWORD,
+      dbDatabase: process.env.MONGO_INITDB_DATABASE,
+      dbUrl: process.env.MONGO_URL,
+      x: 1,
+    };
+    return res;
   }
 }
