89b5912541a48090a1e610f099a16be83ddd37ba
Kerolous Amged <kerolous.amged@nagwa.com>
2024-06-14 21:13:22 +0300

    refactor: dont need to validate login input

--- backend/src/auth/dtos/sigin-user.dto.ts
+++ backend/src/auth/dtos/sigin-user.dto.ts
@@ -1,28 +1,6 @@
-import { IsEmail, IsNotEmpty, Length, Matches } from 'class-validator';
 import { User } from '../../users/entities/user';
-import {
-  MAX_PASSWORD_LENGTH,
-  MESSAGES,
-  MIN_PASSWORD_LENGTH,
-  REGEX,
-} from 'src/users/constants/user.validation';
 
 export class SigninUserDTO extends User {
-  @IsNotEmpty()
-  @IsEmail(
-    {},
-    {
-      message: MESSAGES.EMAIL_INVALID,
-    },
-  )
   email: string;
-
-  @IsNotEmpty()
-  @Length(MIN_PASSWORD_LENGTH, MAX_PASSWORD_LENGTH, {
-    message: MESSAGES.PASSWORD_LENGTH_INVALID,
-  })
-  @Matches(REGEX.PASSWORD, {
-    message: MESSAGES.PASSWORD_FORMAT_INVALID,
-  })
   password: string;
 }
