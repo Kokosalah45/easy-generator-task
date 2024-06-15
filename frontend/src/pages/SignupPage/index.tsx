a47e87ec660a9b0d9bbf2b965f780934c9b87f8e
Kerolous Amged <kerolous.amged@nagwa.com>
2024-06-15 06:04:46 +0300

    chore: finish the signup page

---
+++ frontend/src/pages/SignupPage/index.tsx
@@ -0,0 +1,16 @@
+import PageLayout from '@/components/layout/PageLayout';
+
+const SignupPage = () => {
+  return (
+    <PageLayout>
+      <div className="grid place-items-center">
+        <div className="text-center">
+          <h1>Sign up</h1>
+          <p>Sign up form goes here</p>
+        </div>
+      </div>
+    </PageLayout>
+  );
+};
+
+export default SignupPage;
